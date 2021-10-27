5de31b83e122b7d5884c5507166d7f1d5109e42c
seven <273217471@qq.com>
2021-10-27 17:52:41 +0800

    chore: build

--- dist/htmls/genText.d.ts
+++ dist/htmls/genText.d.ts
@@ -1,4 +1,5 @@
-import { CellHyperlinkValue, CellRichTextValue } from "exceljs";
+import { Cell, CellHyperlinkValue, CellRichTextValue } from "exceljs";
 export declare function genHyperLink(value: CellHyperlinkValue): string;
 export declare function genRichText(value: CellRichTextValue): string;
+export declare function genForMulaValue(cell: Cell): string;
 export declare function genDate(value: Date): string;
