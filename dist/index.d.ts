7fc7fe0a30b69ba56629a7fe4395661c99770dcf
seven <273217471@qq.com>
2021-12-23 11:23:14 +0800

    chore: build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,6 @@
 import { XlsxData, XlsxOptions } from "./types";
 export declare function xlsx2Html(data: XlsxData, options?: XlsxOptions): Promise<string | ArrayBuffer | string[] | Promise<ArrayBuffer>[]>;
+declare const _default: {
+    xlsx2Html: typeof xlsx2Html;
+};
+export default _default;
